0b460a00a95053052104b41478df65d39eb2c415
dantezerega <dante.other@gmail.com>
2026-04-20 17:07:06 -0400

    confirm

--- script.js
+++ script.js
@@ -5,10 +5,19 @@ function toggleTheme() {
 }
 
 function hiName() {
-    var name = prompt("What is your name?");
+    let name = prompt("What is your name?");
     if (name) {
         alert("Hi " + name + "!");
     } else {
         alert("Hi there!");
+    }
+}
+
+function confirmFunc() {
+    let response = confirm("Are you old enought to buy alcohol?");
+    if (response) {
+        alert("You can buy alcohol!");
+    } else {
+        alert("You cannot buy alcohol!");
     }
 }
